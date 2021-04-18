39644b39f4a61aa1d6d70592fb97e55f9c1a2dc6
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-04-18 13:01:57 -0400

    added delete, adding more songs

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -28,3 +28,11 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        song = Song.objects.all()
+        serializer = SongSerializer(data=request.data)
+        if serializer.is_valid():
+            song.delete()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
