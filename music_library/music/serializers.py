740c6ab314ce58086ccc2e3128711d4eacbe2363
Sebastian <maxwellsebas@gmail.com>
2021-06-09 22:06:46 -0400

    added id to fields in serializer

--- music_library/music/serializers.py
+++ music_library/music/serializers.py
@@ -2,24 +2,25 @@ from rest_framework import serializers
 from .models import Song
 
 
-# class SongSerializer(serializers.ModelSerializer):
-# class Meta:
-#     model = Song
-#     fields = ['id', 'title', 'artist', 'album', 'release_date']
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'artist', 'album', 'release_date']
 
-class SongSerializer(serializers.Serializer):
-    title = serializers.CharField(required=True, allow_blank=False, max_length=50)
-    artist = serializers.CharField(required=True, allow_blank=False, max_length=50)
-    album = serializers.CharField(required=True, allow_blank=False, max_length=50)
-    release_date = serializers.DateField(allow_null=False)
+# class SongSerializer(serializers.Serializer):
+#     title = serializers.CharField(required=True, allow_blank=False, max_length=50)
+#     artist = serializers.CharField(required=True, allow_blank=False, max_length=50)
+#     album = serializers.CharField(required=True, allow_blank=False, max_length=50)
+#     release_date = serializers.DateField(allow_null=False)
 
-    def create(self, validated_data):
-        return Song.objects.create(**validated_data)
+    # def create(self, validated_data):
+    #     return Song.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('name', instance.title)
-        instance.artist = validated_data.get('artist', instance.artist)
-        instance.album = validated_data.get('artist', instance.album)
-        instance.release_date = validated_data.get('release_date', instance.release_date)
-        instance.save()
-        return instance
+    # def update(self, instance, validated_data):
+    #     instance.id= validated_data.get('id', instance.id)
+    #     instance.title = validated_data.get('name', instance.title)
+    #     instance.artist = validated_data.get('artist', instance.artist)
+    #     instance.album = validated_data.get('artist', instance.album)
+    #     instance.release_date = validated_data.get('release_date', instance.release_date)
+    #     instance.save()
+    #     return instance
--- music_library/music/views.py
+++ music_library/music/views.py
@@ -23,13 +23,6 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # def put(self, request):
-    #     serializer = SongSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class SongDetail(APIView):
     def get_object(self, pk):
